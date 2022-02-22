a62e79e5bf25c0335234bb2b4cff8e6f8532e6e0
ydy <ydy@123.com>
2022-02-22 20:22:55 +0800

    master commit2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,6 +4,8 @@ public class test {
         System.out.println("second");
         System.out.println("third");
         System.out.println("hot -fix ");
+        System.out.println("master test ");
+
     }
 }
 
