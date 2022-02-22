2a62e362f4df5682d3b49c19ce86259c64056f02
ydy <ydy@123.com>
2022-02-22 20:13:35 +0800

    third

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
+        System.out.println("third");
     }
 }
 
